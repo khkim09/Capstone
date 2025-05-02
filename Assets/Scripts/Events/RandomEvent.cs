1a501320f5f723c84d5c2f9527f2ea9db58610ea
HyukJaeKwon1 <gurwo5332@gmail.com>
2025-05-02 15:56:07 +0900

    퀘스트 버그 수정

--- Assets/Scripts/Events/RandomEvent.cs
+++ Assets/Scripts/Events/RandomEvent.cs
@@ -1,6 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 이벤트 종류
+/// </summary>
+public enum EventType
+{
+    ShipEvent,
+    PlanetEvent,
+    CosmicWonder
+}
+
 [CreateAssetMenu(fileName = "New Event", menuName = "Game/Random Event")]
 public class RandomEvent : ScriptableObject
 {
@@ -8,6 +18,7 @@ public class RandomEvent : ScriptableObject
     public string eventTitle;
     public string eventDescription;
     public Sprite eventImage;
+    public EventType eventType;
 
     public List<EventChoice> choices = new();
 }
