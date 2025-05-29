c52b306c635af1e208631206798dfb76af5ddf26
Cheshire75 <47998568+Cheshire75@users.noreply.github.com>
2025-05-29 23:49:27 +0900

    파워시스템/룸아이콘 인터랙션 고장난거 수정

--- Assets/Scripts/Ship/ShipSystems/PowerSystem.cs
+++ Assets/Scripts/Ship/ShipSystems/PowerSystem.cs
@@ -51,7 +51,7 @@ public class PowerSystem : ShipSystem
         {
             if (powerUsing > powerCapacity)
             {
-                if (room.GetIsPowered())
+                if (room.GetIsPowered() && room.roomType!=RoomType.Power)
                 {
                     room.BlackOut();
                     powerUsing -= room.GetPowerConsumption();
@@ -59,10 +59,10 @@ public class PowerSystem : ShipSystem
             }
             else
             {
-                parentShip.RecalculateAllStats();
-                return;
+                break;
             }
         }
+        parentShip.RecalculateAllStats();
     }
 
     /// <summary>
